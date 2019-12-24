472fd60e175b75e981b70547e94d216745eb1742
Peter Dimov <pdimov@gmail.com>
2019-12-25 01:33:40 +0200

    Add install layout variables

--- include/BoostRoot.cmake
+++ include/BoostRoot.cmake
@@ -3,6 +3,7 @@
 # See accompanying file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt
 
 include(BoostMessage)
+include(GNUInstallDirs)
 
 if(NOT BOOST_ENABLE_CMAKE)
   message(FATAL_ERROR
@@ -11,16 +12,58 @@ if(NOT BOOST_ENABLE_CMAKE)
     "(Boost.Build) to build and install Boost.")
 endif()
 
+# --with-<library>
 set(BOOST_INCLUDE_LIBRARIES "" CACHE STRING "List of libraries to build (default: all but excluded and incompatible)")
+
+# --without-<library>
 set(BOOST_EXCLUDE_LIBRARIES "" CACHE STRING "List of libraries to exclude from build")
+
 set(BOOST_INCOMPATIBLE_LIBRARIES beast;callable_traits;compute;gil;hana;hof;safe_numerics;serialization;yap CACHE STRING "List of libraries with incompatible CMakeLists.txt files")
+
+# --layout
+
+if(WIN32)
+  set(__boost_default_layout "versioned")
+else()
+  set(__boost_default_layout "system")
+endif()
+
+set(BOOST_INSTALL_LAYOUT ${__boost_default_layout} CACHE STRING "Installation layout (versioned, tagged, or system)")
+set_property(CACHE BOOST_INSTALL_LAYOUT PROPERTY STRINGS versioned tagged system)
+
+# --libdir
+set(BOOST_INSTALL_LIBDIR "${CMAKE_INSTALL_LIBDIR}" CACHE STRING "Installation directory for library files")
+
+# --includedir
+
+if(BOOST_INSTALL_LAYOUT STREQUAL "versioned")
+  set(__boost_header_subdir "/${Boost_VERSION_MAJOR}_${Boost_VERSION_MINOR}")
+else()
+  set(__boost_header_subdir "")
+endif()
+
+set(BOOST_INSTALL_INCLUDEDIR "${CMAKE_INSTALL_INCLUDEDIR}${__boost_header_subdir}" CACHE STRING "Installation directory for header files")
+
+# --cmakedir
+set(BOOST_INSTALL_CMAKEDIR "${BOOST_INSTALL_LIBDIR}/cmake" CACHE STRING "Installation directory for CMake configuration files")
 
 if(CMAKE_SOURCE_DIR STREQUAL Boost_SOURCE_DIR)
 
   include(CTest)
 
-  if(WIN32 AND NOT CMAKE_RUNTIME_OUTPUT_DIRECTORY)
-    set(CMAKE_RUNTIME_OUTPUT_DIRECTORY "${CMAKE_BINARY_DIR}/bin")
+  # --stagedir
+  set(BOOST_STAGEDIR "${CMAKE_CURRENT_BINARY_DIR}/stage" CACHE STRING "Build output directory")
+
+  if(NOT CMAKE_RUNTIME_OUTPUT_DIRECTORY)
+    set(CMAKE_RUNTIME_OUTPUT_DIRECTORY "${BOOST_STAGEDIR}/bin")
+  endif()
+
+  if(NOT CMAKE_LIBRARY_OUTPUT_DIRECTORY)
+    set(CMAKE_LIBRARY_OUTPUT_DIRECTORY "${BOOST_STAGEDIR}/lib")
+  endif()
+
+  if(NOT CMAKE_ARCHIVE_OUTPUT_DIRECTORY)
+    set(CMAKE_ARCHIVE_OUTPUT_DIRECTORY "${BOOST_STAGEDIR}/lib")
   endif()
 
 endif()
