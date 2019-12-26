0ceab8fdfa06c9fb5b14c06976e3a8779ed19c36
Peter Dimov <pdimov@gmail.com>
2019-12-27 01:49:49 +0200

    Fix indentation in BoostTestJamfile

--- include/BoostTestJamfile.cmake
+++ include/BoostTestJamfile.cmake
@@ -11,49 +11,49 @@ include(BoostMessage)
 
 function(boost_test_jamfile)
 
-    cmake_parse_arguments(_ "" "FILE;PREFIX" "LIBRARIES;LINK_LIBRARIES" ${ARGN})
+  cmake_parse_arguments(_ "" "FILE;PREFIX" "LIBRARIES;LINK_LIBRARIES" ${ARGN})
 
-    if(__UNPARSED_ARGUMENTS)
-        message(AUTHOR_WARNING "boost_test_jamfile: extra arguments ignored: ${__UNPARSED_ARGUMENTS}")
-    endif()
+  if(__UNPARSED_ARGUMENTS)
+    message(AUTHOR_WARNING "boost_test_jamfile: extra arguments ignored: ${__UNPARSED_ARGUMENTS}")
+  endif()
 
-    if(__LIBRARIES)
-        boost_message(DEBUG "boost_test_jamfile: LIBRARIES is deprecated, use LINK_LIBRARIES")
-    endif()
+  if(__LIBRARIES)
+    boost_message(DEBUG "boost_test_jamfile: LIBRARIES is deprecated, use LINK_LIBRARIES")
+  endif()
 
-    if(DEFINED BUILD_TESTING AND NOT BUILD_TESTING)
-        return()
-    endif()
+  if(DEFINED BUILD_TESTING AND NOT BUILD_TESTING)
+    return()
+  endif()
 
-    file(STRINGS ${__FILE} data)
+  file(STRINGS ${__FILE} data)
 
-    set(types compile compile-fail link link-fail run run-fail)
+  set(types compile compile-fail link link-fail run run-fail)
 
-    foreach(line IN LISTS data)
-        if(line)
+  foreach(line IN LISTS data)
+    if(line)
 
-            string(REGEX MATCHALL "[^ ]+" ll ${line})
+      string(REGEX MATCHALL "[^ ]+" ll ${line})
 
-            if(ll)
-                list(GET ll 0 e0)
+      if(ll)
+        list(GET ll 0 e0)
 
-                if(e0 IN_LIST types)
+        if(e0 IN_LIST types)
 
-                    list(LENGTH ll lln)
+          list(LENGTH ll lln)
 
-                    if(NOT lln EQUAL 2)
+          if(NOT lln EQUAL 2)
 
-                        boost_message(DEBUG "boost_test_jamfile: Jamfile line ignored: ${line}")
+            boost_message(DEBUG "boost_test_jamfile: Jamfile line ignored: ${line}")
 
-                    else()
+          else()
 
-                        list(GET ll 1 e1)
-                        boost_test(PREFIX ${__PREFIX} TYPE ${e0} SOURCES ${e1} LINK_LIBRARIES ${__LIBRARIES} ${__LINK_LIBRARIES})
+            list(GET ll 1 e1)
+            boost_test(PREFIX ${__PREFIX} TYPE ${e0} SOURCES ${e1} LINK_LIBRARIES ${__LIBRARIES} ${__LINK_LIBRARIES})
 
-                    endif()
-                endif()
-            endif()
+          endif()
         endif()
-    endforeach(line)
+      endif()
+    endif()
+  endforeach(line)
 
 endfunction(boost_test_jamfile)
